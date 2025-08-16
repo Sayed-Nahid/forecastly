bd43cc2b0ccf338934ca46900250819f66ba1e40
Sayed Nahid <nahid15-3849@diu.edu.bd>
2025-08-16 22:38:07 +0600

    working on Bloc(state Management

---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:weather/weather.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState>{
+  WeatherBlocBloc() : super(WeatherBlocInitial()){
+    on<FetchWeather>((event, emit) {
+
+    });
+  }
+}
