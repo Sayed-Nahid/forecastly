4cc179ca8e234f56a015d6cdb0551a888ce989f2
Sayed Nahid <nahid15-3849@diu.edu.bd>
2025-08-17 11:43:44 +0600

    project is ready to use API key

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forecastly/bloc/weather_bloc_bloc.dart';
 import 'package:forecastly/screens/home_screen.dart';
 
 void main() {
@@ -12,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: BlocProvider<WeatherBlocBloc>(
+        create: (context) => WeatherBlocBloc()..add(FetchWeather()),
+        child: const HomeScreen(),
+      )
     );
   }
 }
